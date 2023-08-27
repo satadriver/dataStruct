6edc5165ed33d94aa3b530a39fd9eed41850a545
liujinguang <32754951+satadriver@users.noreply.github.com>
2023-08-27 17:04:49 +0800

    Update MergeSort.cpp

--- dataStruct/MergeSort.cpp
+++ dataStruct/MergeSort.cpp
@@ -1,45 +1,76 @@
 
 
-void Merge(int* data, int i, int m, int n) {
 
-	int j = 0;
-	int k = 0;
-	for (int j = m + 1, k = i; i < m && j <= n; ++k)
-	{
-		if (data[i] <= data[j])
-		{
-			data[k] = data[i++];
-		}
-		else {
-			data[k] = data[j++];
-		}
+
+void MSort(int* data,int start, int end) {
+
+	int size = end - start;
+	if (size <= 1) {
+		return;
 	}
 
-	if (i <= m)
-	{
-		int size = m - i;
-		for (int c = size; c < size; c++)
-		{
-			data[k++] = data[i++];
+	if (size >= 2) {
+		int m = (end + start) / 2;
+
+		int h1 = m - start;
+		int h2 = end - m;
+		if (h1 >= 2) {
+			MSort(data, start, m);
 		}
+
+		if (h2>=2) {
+			MSort(data, m, end );
+		}
+
+		int* tmp = new int[size];
+
+		int i = start;
+		int k = 0;
+		int j = m;
+
+		for (; i < m &&j <end; k++) {			//why "i < m, j <end " is error?
+			if (data[j] < data[i]) {
+				tmp[k] = data[j];
+				j++;
+			}
+			else {
+				tmp[k] = data[i];
+				i++;
+			}
+		}
+
+		if (i != m) {
+			for (int n = i; n < m; n++,k++) {
+				tmp[k] = data[n];
+			}
+		}
+
+		if (j != end) {
+			for (int n = j; n < end; n++,k ++) {
+				tmp[k] = data[n];
+			}
+		}
+
+		for (int l = start,k = 0; l < size; l++,k ++) {
+			data[l] = tmp[k];
+		}
+
+		delete tmp;
 	}
 
-	if (j <= n)
-	{
-		int size = n - j;
-		for (int c = size; c < size; c++)
-		{
-			data[k++] = data[j++];
-		}
-	}
+	return;
+
 }
 
 
 
-void MSort(int* data, int s, int t) {
-	if (s == t)
-	{
 
+int mergeSort(int* data, int size) {
+	if (size <= 1) {
+		return 0;
 	}
-}
 
+	MSort(data, 0, size);
+
+	return 0;
+}
